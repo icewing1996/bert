31dfb56f595525b5494a2af8d3072bf40d8818d3
Chiehmin Wei <cmw025@bucknell.edu>
2018-12-06 12:55:46 +0000

    all pits

--- run_classifier.py
+++ run_classifier.py
@@ -331,15 +331,15 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   # tokens are attended to.
   input_mask = [1] * len(input_ids)
 
-  tf.logging.info("input_ids: %s" % str(len(input_ids)))
-  tf.logging.info("label_ids: %s" % str(len(label_ids)))
-  tf.logging.info("input_mask: %s" % str(len(input_mask)))
-  tf.logging.info("segment_ids: %s" % str(len(segment_ids)))
+  # tf.logging.info("input_ids: %s" % str(len(input_ids)))
+  # tf.logging.info("label_ids: %s" % str(len(label_ids)))
+  # tf.logging.info("input_mask: %s" % str(len(input_mask)))
+  # tf.logging.info("segment_ids: %s" % str(len(segment_ids)))
 
-  tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-  tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-  tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-  tf.logging.info("labels: %s (id = %s)" % (example.label, " ".join([str(x) for x in label_ids])))
+  # tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+  # tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+  # tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+  # tf.logging.info("labels: %s (id = %s)" % (example.label, " ".join([str(x) for x in label_ids])))
 
   assert len(input_ids) == len(label_ids) == len(input_mask) == len(segment_ids)
   if len(input_ids) > max_seq_length:
