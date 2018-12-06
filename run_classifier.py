e8312de2c072f44e59c33893a0050a1419b9c0e5
Chiehmin Wei <cmw025@bucknell.edu>
2018-12-06 01:16:31 +0000

    lets go

--- run_classifier.py
+++ run_classifier.py
@@ -305,7 +305,7 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
 
   bert_tokens.append("[CLS]")
   for orig_token in orig_tokens:
-    token_start_mask[len(bert_tokens)] = 1
+    token_start_mask[0, len(bert_tokens)] = 1
     # token_start_idxs.append(len(bert_tokens))
     bert_tokens.extend(tokenizer.tokenize(orig_token))
   bert_tokens.append("[SEP]")
